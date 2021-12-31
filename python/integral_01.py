74e02e1ceb0457f461ab460211193a1c11c1b4a3
Sultanow <sultanow@outlook.com>
2021-12-31 10:50:58 +0100

    update

--- python/integral_01.py
+++ python/integral_01.py
@@ -1,10 +1,8 @@
 from sympy import *
 from sympy.printing.latex import print_latex
 
-t = Symbol('t')
 x = Symbol('x')
-a = Symbol('a')
-b = Symbol('b')
 
-print_latex(integrate(a*exp(x)/t, (x, 0, oo)))
-print_latex(integrate(exp((-a*t**2+I*b*t)/(3*t**2+1)+I*t*x)*(x/(3*t**2+1)), (t, -oo, oo), (x, 0, oo)))
+print_latex(integrate(exp(-x)*sqrt(cos(x))/(sqrt(cos(x))+sqrt(sin(x))), (x, 0, pi/2)))
+print_latex(integrate(sqrt(x)+x**3, x))
+print_latex(integrate(x**(x**x), (x, 0, 1)))
